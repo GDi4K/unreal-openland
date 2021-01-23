1d1f18a68bc1589aae2007b496b4100263d60360
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2021-01-23 23:44:26 +0530

    Introduce a clean.bat to clean up before packaging
    This will remove some auto-generated directories.
    So, we can package files which we really need.

---
+++ clean.bat
@@ -0,0 +1,10 @@
+cd OpenLand
+rmdir /S /Q DerivedDataCache
+rmdir /S /Q Intermediate
+rmdir /S /Q Saved
+
+cd Content
+rmdir /S /Q Collections
+rmdir /S /Q Developers
+rmdir /S /Q StarterContent
+
--- setup.bat
+++
@@ -1,2 +0,0 @@
-cd Demo\Content
-mklink /D OpenLand ..\..\OpenLand
