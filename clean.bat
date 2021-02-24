93fc3435ec673870062c0bdf92c4738bfd0f7798
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2021-02-24 23:46:03 +0530

    Update scripts & added marketplace related files

--- clean.bat
+++ clean.bat
@@ -8,3 +8,14 @@ rmdir /S /Q Collections
 rmdir /S /Q Developers
 rmdir /S /Q StarterContent
 
+cd ..\..\
+
+cd OpenLandLegacy
+rmdir /S /Q DerivedDataCache
+rmdir /S /Q Intermediate
+rmdir /S /Q Saved
+
+cd Content
+rmdir /S /Q Collections
+rmdir /S /Q Developers
+rmdir /S /Q StarterContent
